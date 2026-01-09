c8a5e5387c21328a089de9b5c85426d3f190dbf4
Manishbhai9350 <keshraram294@gmail.com>
2026-01-09 20:22:17 +0530

    Analyze Resume Form UI

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useCallback, useState, type ActionDispatch, type SetStateAction } from "react";
+import { useDropzone } from "react-dropzone";
+import { formatSize } from "~/utils";
 
-const FileUploader = () => {
-  return (
-    <div className='w-full gradient-border'>FileUploader</div>
-  )
+interface FileUploaderProps {
+  file: File | null;
+  setFile: React.Dispatch<React.SetStateAction<File | null>>;
 }
 
-export default FileUploader
+const FileUploader = ({ file,setFile }:FileUploaderProps) => {
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0]
+
+    if(!file) return null;
+
+    setFile(file)
+
+
+  }, []);
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles} =
+    useDropzone({ onDrop });
+
+  return (
+    <div
+      className={`w-full rounded-2xl transition-colors duration-150 ${isDragActive ? "bg-sky-600" : "bg-sky-400"}`}
+      {...getRootProps()}
+    >
+      <input {...getInputProps()} />
+      {file ? (
+        <div onClick={e => e.stopPropagation()} className="w-full px-4 flex justify-between items-center relative gradient-border text-center text-white">
+          <img
+            className="h-10 "
+            src="/images/pdf.png"
+          />
+          <div className="flex flex-col justify-center items-center">
+            <p className="leading-6 text-lg max-w-[40%] truncate text-center">{ file.name }</p>
+            <p className="opacity-80 text-sm">{ formatSize(file.size) }</p>
+          </div>
+          
+          <div onClick={e => setFile(null)} className="h-7 w-7 shrink-0 cursor-pointer flex justify-center items-center rounded-full bg-red-500">
+            <img
+              className="h-5 w-5"
+              src="/icons/cross.svg"
+              />
+          </div>
+        </div>
+      ) : (
+        <div className="w-full cursor-pointer gradient-border text-center text-white">
+          <p>Select or Drag and Drop your Resume</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useMemo, useRef, useState, type FormEvent } from "react";
 import FileUploader from "~/components/FileUploader";
 import Navbar from "~/components/Navbar";
 import TileBg from "~/components/TileBg";
@@ -6,41 +6,81 @@ import TileBg from "~/components/TileBg";
 const upload = () => {
   const parentRef = useRef<HTMLDivElement>(null);
 
+  const [file, setFile] = useState<File | null>(null);
+  const [companyName, setCompanyName] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
+
+  const isValidForm = useMemo(
+    () => companyName && jobTitle && jobDescription && file,
+    [companyName, jobTitle, jobDescription, file]
+  );
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (!isValidForm) return;
+  }
+
   return (
-    <main
-      ref={parentRef}
-      className="upload-page w-screen h-auto relative"
-    >
-        <Navbar />
-        <TileBg parentRef={parentRef} />
+    <main ref={parentRef} className="upload-page w-screen h-auto relative">
+      <Navbar />
+      <TileBg parentRef={parentRef} />
 
-        <section className="main-section w-full min-h-screen h-fit relative z-20 px-5 ">
-            <div className="page-heading">
-                <h1>Smart feedback for your dream job.</h1>
-                <h2>Drop your resume for an ATS score and improvement tips</h2>
-            </div>
+      <section className="main-section w-full min-h-screen h-fit relative z-20 px-5 ">
+        <div className="page-heading">
+          <h1>Smart feedback for your dream job.</h1>
+          <h2>Drop your resume for an ATS score and improvement tips</h2>
+        </div>
 
-            <form action="" className="upload-form">
-                <div className="form-div">
-                    <label htmlFor="company-name">Company Name</label>
-                    <input type="text" placeholder="Company Name" name="company-name" id="company-name" />
-                </div>
-                <div className="form-div">
-                    <label htmlFor="job-title">Job Title</label>
-                    <input type="text" placeholder="Job Title" name="job-title" id="job-title" />
-                </div>
-                <div className="form-div">
-                    <label htmlFor="job-description">Job Description</label>
-                    <textarea rows={5} placeholder="Job Description" name="job-description" id="job-description" />
-                </div>
-                <div className="form-div">
-                    <label htmlFor="upload-resume">Upload Resume</label>
-                    <FileUploader />
-                </div>
-            </form>
-
-        </section>
-
+        <form onSubmit={onSubmit} className="upload-form">
+          <div className="form-div">
+            <label htmlFor="company-name">Company Name</label>
+            <input
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+              type="text"
+              placeholder="Company Name"
+              name="company-name"
+              id="company-name"
+            />
+          </div>
+          <div className="form-div">
+            <label htmlFor="job-title">Job Title</label>
+            <input
+              value={jobTitle}
+              onChange={(e) => setJobTitle(e.target.value)}
+              type="text"
+              placeholder="Job Title"
+              name="job-title"
+              id="job-title"
+            />
+          </div>
+          <div className="form-div">
+            <label htmlFor="job-description">Job Description</label>
+            <textarea
+              value={jobDescription}
+              onChange={(e) => setJobDescription(e.target.value)}
+              rows={5}
+              placeholder="Job Description"
+              name="job-description"
+              id="job-description"
+            />
+          </div>
+          <div className="form-div">
+            <label htmlFor="upload-resume">Upload Resume</label>
+            <FileUploader file={file} setFile={setFile} />
+          </div>
+          <div className="form-div">
+            <button
+              type="submit"
+              className={`w-full ${isValidForm ? 'bg-blue-600' : 'bg-blue-300'} cursor-pointer transition-colors duration-300 text-white py-3 rounded-3xl text-center`}
+            >
+              Analyze Resume
+            </button>
+          </div>
+        </form>
+      </section>
     </main>
   );
 };
