7d1f41843c60ed846bba4d38a1df932434742dd5
Manishbhai9350 <keshraram294@gmail.com>
2026-01-10 21:27:26 +0530

    Showing Resume AI Feedback

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
+import ATS from "~/components/feedback/ATS";
 import Details from "~/components/feedback/Details";
 import Summary from "~/components/feedback/Summary";
 import { usePuterStore } from "~/lib/puter";
@@ -42,7 +43,12 @@ const resume = () => {
       setImageUrl(imgUrl);
       setFeedback(Resume.feedback);
 
-      console.log(Resume.feedback.ATS)
+      let parsedFeedback = Resume.feedback;
+      if(typeof parsedFeedback == 'string'){
+        parsedFeedback = JSON.parse(parsedFeedback)
+    }
+    console.log(parsedFeedback)
+    setFeedback(parsedFeedback)
 
       setFetchingResume(false);
     }
@@ -54,14 +60,26 @@ const resume = () => {
 
   return (
     <main className="resume-page w-full h-auto !pt-0 m-0">
-      <nav className="w-full flex h-12 justify-between items-center bg-white shadow-2xl px-6">
+      <nav className="w-full absolute top-0 z-20 flex h-12 justify-between items-center bg-white shadow-2xl px-6">
         <Link to={"/"} className="back-button">
           <img src="/icons/back.svg" alt="back" />
         </Link>
       </nav>
 
+      {
+        FetchingResume && (
+            <div className="fetching-img h-screen w-full flex justify-center items-center">
+                <img 
+                  src="/images/resume-scan-2.gif"
+                  alt="fetching-resume"
+                  className="h-[400px] object-cover"
+                />
+            </div>
+        )
+      }
+
       {feedback && imageUrl && (
-        <div className="resume-section p-4 relative gap-3 flex flex-row max-sm:flex-col-reverse">
+        <div className="resume-section p-4 pt-12 h-screen overflow-y-scroll gap-3 flex flex-row max-sm:flex-col-reverse">
           <div className="resume flex-1 w-full h-screen sticky top-0">
             <div className="h-[90%] w-full flex justify-center">
               <img
@@ -72,12 +90,15 @@ const resume = () => {
             </div>
           </div>
           <div className="summary flex-1">
-            <h2>Resume Summary</h2>
+            <h2 className="font-semibold text-2xl">Resume Summary</h2>
             <div className="summary">
               <Summary feedback={feedback} />
             </div>
             <div className="details">
-                <Details feedback={feedback} />
+              <Details feedback={feedback} />
+            </div>
+            <div className="ats">
+                <ATS score={feedback.ATS.score} suggestions={feedback.ATS.tips}  />
             </div>
           </div>
         </div>
