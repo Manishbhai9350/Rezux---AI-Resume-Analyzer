3b7684fbf02c7a267ed4787be70f24436fe3dda6
Manishbhai9350 <keshraram294@gmail.com>
2026-01-11 15:55:45 +0530

    Showing Real Resumes On Homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({
   feedback,
@@ -8,24 +10,66 @@ const ResumeCard = ({
   companyName,
   jobTitle,
 }: Resume) => {
+
+  const { kv,fs } = usePuterStore()
+  const [FetchingResumeImage, setFetchingResumeImage] = useState(false)
+  const [ImageUrl, setImageUrl] = useState('')
+
+  useEffect(() => {
+    
+    const FetchResumeImage = async () => {
+      setFetchingResumeImage(true)
+
+      const imgBlob =  await fs.read(imagePath)
+
+      if(!imgBlob) return;
+
+      const imgUrl = URL.createObjectURL(imgBlob)
+
+      if(!imgUrl) return;
+
+      setImageUrl(imgUrl)
+
+      setFetchingResumeImage(false)
+    }
+
+    FetchResumeImage()
+  
+    return () => {
+      
+    }
+  }, [imagePath])
+  
+
   return (
     <div className="resume-card">
       <div className="resume-card-header">
         <div className="flex flex-col">
-          <h1 className="!text-3xl">{jobTitle}</h1>
-          <p className="text-black/40">{companyName}</p>
+          { jobTitle && <h1 className="!text-xl">{jobTitle}</h1>}
+          { companyName && <p className="text-black/40 text-sm">{companyName}</p>}
         </div>
         <div className="shrink-0">
           <ScoreCircle score={90} />
         </div>
       </div>
-      <div className="resume-image w-full h-full overflow-hidden rounded-sm">
-        <img
-          className="w-full h-auto object-cover"
-          src={imagePath}
-          alt="resume-image"
-        />
-      </div>
+      {
+        FetchingResumeImage ? (
+          <div className="fetching w-full flex justify-center items-center">
+            <img 
+              src="/images/resume-scan-2.gif"
+              className="w-[200px]"
+            />
+          </div>
+        ) : (
+        <div className="resume-image w-full h-full overflow-hidden rounded-sm">
+          {ImageUrl && <img
+            className="w-full h-auto object-cover"
+            src={ImageUrl}
+            alt="resume-image"
+          />}
+        </div>
+        )
+      }
     </div>
   );
 };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,9 +3,9 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { usePuterStore } from "~/lib/puter";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -18,16 +18,35 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { isLoading , auth } = usePuterStore();
+  const { isLoading, auth, fs, kv } = usePuterStore();
 
   const navigate = useNavigate();
   const containerRef = useRef<HTMLDivElement>(null);
+  const [IsFetchingResumes, setIsFetchingResumes] = useState(false);
+  const [Resumes, setResumes] = useState<null | Resume[]>(null);
 
   useEffect(() => {
     if (!isLoading && !auth.isAuthenticated) {
       navigate("/auth?next=/");
     }
-  }, [auth.isAuthenticated,isLoading]);
+
+    setIsFetchingResumes(true);
+
+    const FetchResumes = async () => {
+      const resumes = (await kv.list("resume:*", true)) as KVItem[];
+
+      if (!resumes || !resumes?.length) return;
+
+      const parsed = resumes.map(
+        (resume) => JSON.parse(resume.value) as Resume
+      );
+      setResumes(parsed);
+
+      setIsFetchingResumes(false);
+    };
+
+    FetchResumes();
+  }, [auth.isAuthenticated, isLoading]);
 
   return (
     <main className="main w-screen h-auto relative">
@@ -38,16 +57,35 @@ export default function Home() {
         className="main-section max-w-[1400px] relative z-10 px-4"
       >
         <div className="section-headings">
-          <h1 className="text-center">Analyze Your Resume. Beat the ATS.</h1>
-          <h2 className="text-center">
+          <h1 className="text-center !text-4xl">
+            Analyze Your Resume. Beat the ATS.
+          </h1>
+          <h2 className="text-center !text-sm">
             Get instant, AI-powered insights to improve keyword match,
             structure, and readability.
           </h2>
         </div>
 
-        <div className="resumes-section w-full">
-          {resumes &&
-            resumes.map((resume) => <ResumeCard key={resume.id} {...resume} />)}
+        {IsFetchingResumes && (
+          <div className="fetching w-full flex justify-center items-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+        )}
+
+        {!Resumes?.length && (
+          <div className="upload capatilize text-md font-semibold text-gray-600">
+            click{" "}
+            <Link to="/upload" className="navbar-button primary-button w-fit">
+              Upload Resume
+            </Link> {" "}
+            your first resume
+          </div>
+        )}
+
+        <div className="resumes-section w-full flex flex-wrap gap-3">
+          {!IsFetchingResumes &&
+            Resumes &&
+            Resumes.map((resume) => <ResumeCard key={resume.id} {...resume} />)}
         </div>
       </section>
     </main>
